dade3492bc4ad6478e088ef3ae969fed415dc705
cecilepoujade <cecile.poujade@eleve.ensai.fr>
2025-04-07 09:02:03 +0000

    tp

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,27 @@ public class ApiRestController {
     public List<Athlete> allAthletes() {
 
         return athleteService.findAll();
+    }
+
+    /**
+     * Get athletes by id
+     */
+    @GetMapping("/athlete/{id}")
+    public Athlete getAthletesById(@PathVariable("id") Long id) {
+
+        return athleteService.findById(id);
+    }
+
+    /**
+     * Delete athletes by id
+     */
+    @GetMapping("/athlete/{id}")
+    public void deleteAthletesById(@PathVariable("id") Long id) {
+
+        Athlete athlete = athleteService.findById(id);
+        if (athlete != null) {
+            athleteService.deleteById(id);
+        }
     }
 
 }
