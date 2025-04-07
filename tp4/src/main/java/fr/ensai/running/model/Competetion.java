1c8d26202672967af05d2253e99ec1884de647d6
cecilepoujade <cecile.poujade@eleve.ensai.fr>
2025-04-07 10:43:48 +0000

    tp suite

---
+++ tp4/src/main/java/fr/ensai/running/model/Competetion.java
@@ -0,0 +1,30 @@
+package fr.ensai.running.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Competetion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_competition")
+    private long id;
+
+    private String designation;
+    private String city;
+    private LocalDate date;
+    private float distance;
+    private int maxAthletes;
+
+
+
+}
---
+++ tp4/src/main/java/fr/ensai/running/repository/CompetetionRepository.java
@@ -0,0 +1,6 @@
+package fr.ensai.running.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetetionRepository extends JpaRepository<Competition, Long> {
+}
